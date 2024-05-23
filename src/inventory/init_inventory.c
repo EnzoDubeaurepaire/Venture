1c7b45d3d51e16f08393a14b81732bc635e6edcb
AxelDesert <axel.desert@epitech.eu>
2024-05-23 20:01:51 +0200

    feat: adding a pickup function, show inventory and init inventory

--- src/inventory/init_inventory.c
+++ src/inventory/init_inventory.c
@@ -11,24 +11,38 @@ static sfVector2f *init_pos_tab(void)
 {
     sfVector2f *tab = malloc(sizeof(sfVector2f) * 8);
 
-    tab[0] = (sfVector2f){0, 0};
-    tab[1] = (sfVector2f){0, 0};
-    tab[2] = (sfVector2f){0, 0};
-    tab[3] = (sfVector2f){0, 0};
-    tab[4] = (sfVector2f){0, 0};
-    tab[5] = (sfVector2f){0, 0};
-    tab[6] = (sfVector2f){0, 0};
-    tab[7] = (sfVector2f){0, 0};
+    tab[0] = (sfVector2f){722, 970};
+    tab[1] = (sfVector2f){780, 970};
+    tab[2] = (sfVector2f){842, 970};
+    tab[3] = (sfVector2f){906, 970};
+    tab[4] = (sfVector2f){728, 970};
+    tab[5] = (sfVector2f){0, 970};
+    tab[6] = (sfVector2f){0, 970};
+    tab[7] = (sfVector2f){0, 970};
     return tab;
 }
 
-static slot_t **init_slots(void)
+static sfIntRect *init_rect_tab(void)
 {
-    slot_t **slot_tab = malloc(sizeof(slot_t) * 9);
+    sfIntRect *tab = malloc(sizeof(sfIntRect) * 3);
+
+    tab[0] = (sfIntRect){0, 0, 32, 32};
+    tab[1] = (sfIntRect){32, 0, 32, 32};
+    tab[2] = (sfIntRect){64, 0, 32, 32};
+    return tab;
+}
+
+static slot_t **init_slots(sfTexture *items_texture, sfVector2f *pos_tab)
+{
+    slot_t **slot_tab = malloc(sizeof(slot_t *) * 9);
 
     for (int i = 0; i < 8; i++) {
-        slot_tab[i] = malloc(sizeof());
+        slot_tab[i] = malloc(sizeof(slot_t));
+        slot_tab[i]->stored_item = NONE;
+        slot_tab[i]->slot = create_sprite(items_texture,
+        pos_tab[i], (sfVector2f){1.5, 1.5});
     }
+    slot_tab[8] = NULL;
     return slot_tab;
 }
 
@@ -37,10 +51,15 @@ void init_inventory(map_screen_t *map)
     inventory_t *new_inventory;
 
     new_inventory = malloc(sizeof(inventory_t));
+    new_inventory->items_texture = sfTexture_createFromFile(
+    "assets/inventory/items.png", NULL);
     new_inventory->hotbar_texture = sfTexture_createFromFile(
     "assets/inventory/hotbar.png", NULL);
     new_inventory->hotbar = create_sprite(new_inventory->hotbar_texture,
-    (sfVector2f){0, 0}, (sfVector2f){0, 0});
+    (sfVector2f){700, 950}, (sfVector2f){8, 8});
     new_inventory->pos_tab = init_pos_tab();
+    new_inventory->rect_tab = init_rect_tab();
+    new_inventory->slot_tab = init_slots(new_inventory->items_texture,
+         new_inventory->pos_tab);
     map->inventory = new_inventory;
 }
