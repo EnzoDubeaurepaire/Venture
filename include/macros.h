8f3cce2c73d60e281a74c0cbeffe45d26bae15be
Enzo Dubeaurepaire <enzo.dubeaurepaire@epitech.eu>
2024-05-24 19:14:54 +0200

    feat: bridge save

--- include/macros.h
+++ include/macros.h
@@ -18,7 +18,7 @@
     #define STATS_SCREEN 32
     #define SETTINGS_SCREEN 64
     #define DELAY 0.05
-    #define SAVED_ELEMENTS 20
+    #define SAVED_ELEMENTS 21
     #define ENEMIES 20
     #define LAZINESS 30
 
